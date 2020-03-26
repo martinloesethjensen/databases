f1d02c46dd438b188759e5f43c3b4031793ac61f
Martin L. Jensen <martinloesethjensen@gmail.com>
2020-03-26 22:55:56 +0100

    create user in mongodb

--- py-flask-mongodb/app.py
+++ py-flask-mongodb/app.py
@@ -1,11 +1,39 @@
-from flask import Flask
+import json
+
+import pymongo
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
+try:
+    mongo = pymongo.MongoClient(host="localhost", port=27017, serverSelectionTimeoutMS=1000)
+    db = mongo.company
+    mongo.server_info()  # Trigger exception
+except:
+    print("Error connecting to the database")
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('/users', methods=["POST"])
+def create_user():
+    try:
+        user = {
+            "name": request.form['name'],
+            "lastName": request.form['lastName']
+        }
+        db_response = db.users.insert_one(user)
+        db_inserted_id = db_response.inserted_id
+        return Response(
+            response=json.dumps(
+                {
+                    "message": "user created",
+                    "id": f"{db_inserted_id}"
+                }),
+            status=200,
+            mimetype="application/json"
+        )
+    except Exception as ex:
+        print(ex)
+        return "xxxxx"
 
 
 if __name__ == '__main__':
